12a551179cf50b84105540d92760fdfdaf7ed58a
Yousif <haqi.yousef@gmail.com>
2021-07-10 15:23:51 +0300

    again

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,9 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
+app.get('/', (req, res) => { res.send('Hello from Express!')
+
+
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
